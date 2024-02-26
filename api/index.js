86b2fd87f8032ed125241784e90118989ff46a6f
Erazul Islam <erazul89@gmail.com>
2024-02-26 13:42:33 +0600

    connect to the mongodb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose.connect('mongodb+srv://erazul89:Xj8xDO7tXJFq17Zc@taosif.sxba9qz.mongodb.net/mern-blog?retryWrites=true&w=majority&appName=Taosif')
+.then(() => {
+    console.log('MongoDb is connected')
+}).catch(err => {
+    console.log(err)
+})
+
 const app = express();
 
 app.listen(3000, () => {
