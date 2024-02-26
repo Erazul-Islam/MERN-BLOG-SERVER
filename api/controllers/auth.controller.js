4a126670281ae13b6f22692eb6b8a5e551666c55
Erazul Islam <erazul89@gmail.com>
2024-02-26 17:42:12 +0600

    added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,21 +1,27 @@
 import User from "../modules/user.model.js";
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req,res) => {
-    const {userName,email,password} = req.body;
+export const signup = async (req, res, next) => {
+    const { userName, email, password } = req.body;
 
-    if ( !userName || email || !password || userName === '' || email === '' || password === '') {
-        return res.status(400).json({message: 'All fields are required'})
+    if (!userName || email || !password || userName === '' || email === '' || password === '') {
+        next(errorHandler(400, 'All fields are required'))
     }
 
-    const hashedPass = bcryptjs.hashSync(password,10)
+    const hashedPass = bcryptjs.hashSync(password, 10)
 
-    const newUser = new User ({
+    const newUser = new User({
         userName,
         email,
         password: hashedPass,
     })
 
-    await newUser.save();
-    res.json('signup successful')
+    try {
+        await newUser.save();
+        res.json('signup successful')
+    }
+    catch (error){
+        next(error)
+    }
 }
